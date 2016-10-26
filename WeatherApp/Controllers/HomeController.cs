a3b6b390dcfbaaf88ca418914beb400b462d8734
Melissa <hughes.melissa1@gmail.com>
2016-10-25 20:29:42 -0400

    first commit

---
+++ WeatherApp/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeatherApp.Models;
+
+namespace WeatherApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View("WeatherJS");
+        }
+        public ActionResult WeatherJS()
+        {
+            return View();
+        }
+
+        public ActionResult WeatherOrNot()
+        {
+            return View();
+        }
+
+        public JsonResult GetWeather()
+        {
+            Weather weath = new Weather();
+            return Json(weath.getWeatherForecast(), JsonRequestBehavior.AllowGet);
+            //return Json();
+        }
+    }
+}
---
+++ WeatherApp/Models/Weather.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+
+namespace WeatherApp.Models
+{
+    public class Weather
+    {
+        public Object getWeatherForecast()
+        {
+            string url = "http://api.openweathermap.org/data/2.5/weather?q=Cleveland&APPID=2d575e9c17c98627cf13da50433ced10&units=imperial";
+            //synchronous client
+            var client = new WebClient();
+            var content = client.DownloadString(url);
+            var serializer = new JavaScriptSerializer();
+            var jsonContent = serializer.Deserialize<Object>(content);
+            return jsonContent;
+        }
+    }
+}
